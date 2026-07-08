be31d4a5f68fca9c9ae15f30a92aef9cc8e8fa73
YukiAminaka <yukiaminaka28@gmail.com>
2026-07-08 23:29:27 +0900

    add: ルートモジュールを追加

---
+++ terraform/aws/env/prod/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "mobility-digital-twin"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "domain_name" {
+  description = "Domain name for SSL certificate"
+  type        = string
+  default     = "*.ami-works.com"
+}
