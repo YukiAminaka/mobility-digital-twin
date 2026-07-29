30c94f264c48ee2c6192ad531351a277120c4578
YukiAminaka <yukiaminaka28@gmail.com>
2026-07-29 17:40:34 +0900

    fix: Route53モジュールの未使用変数を削除

--- terraform/aws/modules/route53/variables.tf
+++ terraform/aws/modules/route53/variables.tf
@@ -1,13 +1,3 @@
-variable "project_name" {
-  type        = string
-  description = "Project name"
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment name"
-}
-
 variable "domain_name" {
   type        = string
   description = "Domain name for the Route53 zone"
